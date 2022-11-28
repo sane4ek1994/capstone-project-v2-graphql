ecc1af8d4e66e6e9f08d5be020690faf52e1fe1e
Alexandr Naum <mfq07k9cz@gmail.com>
2022-11-28 11:38:28 +0300

    update collectionsMap and added Spinner for categories-preview and category components

--- src/routes/categories-preview/categories-preview.component.jsx
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -1,21 +1,24 @@
-import { useContext, Fragment } from 'react';
+import { useContext, Fragment } from 'react'
 
-import { CategoriesContext } from '../../contexts/categories.context';
-import CategoryPreview from '../../components/category-preview/category-preview.component';
+import { CategoriesContext } from '../../contexts/categories.context'
+import CategoryPreview from '../../components/category-preview/category-preview.component'
+import Spinner from '../../components/spinner/spinner.component'
 
 const CategoriesPreview = () => {
-  const { categoriesMap } = useContext(CategoriesContext);
+  const { categoriesMap, loading } = useContext(CategoriesContext)
 
   return (
     <Fragment>
-      {Object.keys(categoriesMap).map((title) => {
-        const products = categoriesMap[title];
-        return (
-          <CategoryPreview key={title} title={title} products={products} />
-        );
-      })}
+      {loading ? (
+        <Spinner />
+      ) : (
+        Object.keys(categoriesMap).map(title => {
+          const products = categoriesMap[title]
+          return <CategoryPreview key={title} title={title} products={products} />
+        })
+      )}
     </Fragment>
-  );
-};
+  )
+}
 
-export default CategoriesPreview;
+export default CategoriesPreview
--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,32 +1,36 @@
-import { useContext, useState, useEffect, Fragment } from 'react';
-import { useParams } from 'react-router-dom';
+import { useContext, useState, useEffect, Fragment } from 'react'
+import { useParams } from 'react-router-dom'
 
-import ProductCard from '../../components/product-card/product-card.component';
+import ProductCard from '../../components/product-card/product-card.component'
+import Spinner from '../../components/spinner/spinner.component'
 
-import { CategoriesContext } from '../../contexts/categories.context';
+import { CategoriesContext } from '../../contexts/categories.context'
 
-import { CategoryContainer, Title } from './category.styles';
+import { CategoryContainer, Title } from './category.styles'
 
 const Category = () => {
-  const { category } = useParams();
-  const { categoriesMap } = useContext(CategoriesContext);
-  const [products, setProducts] = useState(categoriesMap[category]);
+  const { category } = useParams()
+  const { categoriesMap, loading } = useContext(CategoriesContext)
+  const [products, setProducts] = useState(categoriesMap[category])
 
   useEffect(() => {
-    setProducts(categoriesMap[category]);
-  }, [category, categoriesMap]);
+    setProducts(categoriesMap[category])
+  }, [category, categoriesMap])
 
   return (
     <Fragment>
-      <Title>{category.toUpperCase()}</Title>
-      <CategoryContainer>
-        {products &&
-          products.map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))}
-      </CategoryContainer>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <Fragment>
+          <Title>{category.toUpperCase()}</Title>
+          <CategoryContainer>
+            {products && products.map(product => <ProductCard key={product.id} product={product} />)}
+          </CategoryContainer>
+        </Fragment>
+      )}
     </Fragment>
-  );
-};
+  )
+}
 
-export default Category;
+export default Category
